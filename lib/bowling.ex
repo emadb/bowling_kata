310509eea0df3e68170e0a129cec2edc3280e12e
Emanuele DelBono <emanuele@codiceplastico.com>
2024-03-27 20:42:36 +0100

    refactoring

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,6 +1,6 @@
 defmodule Bowling do
-  def score([10, p3, p4]), do: 10 + p3 + p4
-  def score([10, p3, p4 | tail]), do: 10 + p3 + p4 + score([p3, p4 | tail])
+  def score([10, p2, p3]), do: 10 + p2 + p3
+  def score([10, p2, p3 | tail]), do: 10 + p2 + p3 + score([p2, p3 | tail])
   def score([p1, p2, p3 | tail]) when p1 + p2 == 10,  do: p1 + p2 + p3 + score([p3 | tail])
   def score([p1, p2 | tail]) when p1 + p2 < 10 , do: p1 + p2 + score(tail)
   def score([_|_]), do: 0
