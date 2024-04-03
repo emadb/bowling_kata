18c2337e5c51b29ca580f05cc294dd63a637fc13
Emanuele DelBono <emanuele@codiceplastico.com>
2024-04-03 09:52:00 +0200

    fix a problem with the last shot

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,8 +1,12 @@
 defmodule Bowling do
-  def score([10, p2, p3]), do: 10 + p2 + p3
-  def score([10, p2, p3 | tail]), do: 10 + p2 + p3 + score([p2, p3 | tail])
-  def score([p1, p2, p3 | tail]) when p1 + p2 == 10,  do: p1 + p2 + p3 + score([p3 | tail])
-  def score([p1, p2 | tail]) when p1 + p2 < 10 , do: p1 + p2 + score(tail)
-  def score([_|_]), do: 0
-  def score([]), do: 0
+  def score(game, frame \\ 1)
+
+  def score([p1, p2], 10), do: p1 + p2
+  def score([p1, p2, p3], 10), do: p1 + p2 + p3
+  def score([10, p2, p3 | tail], frame), do: 10 + p2 + p3 + score([p2, p3 | tail], frame + 1)
+
+  def score([p1, p2, p3 | tail], frame) when p1 + p2 == 10,
+    do: p1 + p2 + p3 + score([p3 | tail], frame + 1)
+
+  def score([p1, p2 | tail], frame) when p1 + p2 < 10, do: p1 + p2 + score(tail, frame + 1)
 end
--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -13,18 +13,13 @@ defmodule BowlingTest do
   end
 
   test "one spare" do
-    score = Bowling.score([5,5, 3,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0])
+    score = Bowling.score([5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert score == 16
   end
 
   test "one strike" do
-    score = Bowling.score([10, 3, 4, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0])
+    score = Bowling.score([10, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert score == 24
-  end
-
-  test "quasi perfect game" do
-    score = Bowling.score([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10])
-    assert score == 270
   end
 
   test "perfect game" do
@@ -38,16 +33,36 @@ defmodule BowlingTest do
   end
 
   test "calculates score for a game with all spares" do
-    assert Bowling.score([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]) == 75
-    assert Bowling.score([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]) == 150
+    assert Bowling.score([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]) ==
+             150
   end
 
+  test "9th frame Strike and 10th has points" do
+    assert Bowling.score([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 3, 4]) == 24
+  end
 
-  test "more cases" do
+  test "More cases" do
     assert Bowling.score([6, 2, 6, 2, 2, 5, 5, 5, 0, 9, 9, 0, 6, 4, 10, 0, 0, 0, 0]) == 81
     assert Bowling.score([6, 2, 6, 2, 2, 5, 5, 5, 0, 9, 9, 0, 6, 4, 10, 5, 2, 9, 1, 8]) == 113
     assert Bowling.score([8, 0, 3, 0, 9, 0, 1, 3, 5, 2, 1, 4, 1, 6, 10, 2, 8, 5, 5, 1]) == 89
     assert Bowling.score([9, 0, 4, 3, 5, 4, 6, 4, 5, 4, 4, 4, 8, 0, 7, 3, 1, 7, 4, 2]) == 90
     assert Bowling.score([8, 2, 5, 0, 2, 6, 0, 7, 6, 3, 0, 10, 2, 5, 3, 0, 3, 2, 8, 2, 4]) == 85
+    assert Bowling.score([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) == 20
+    assert Bowling.score([10, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) == 24
+    assert Bowling.score([5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) == 16
+    assert Bowling.score([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]) == 150
+
+    assert Bowling.score([0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0]) ==
+             100
+
+    assert Bowling.score([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]) == 300
+    assert Bowling.score([9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0]) == 90
+    assert Bowling.score([6, 2, 6, 2, 2, 5, 5, 5, 0, 9, 9, 0, 6, 4, 10, 0, 0, 0, 0]) == 81
+    assert Bowling.score([6, 2, 6, 2, 2, 5, 5, 5, 0, 9, 9, 0, 6, 4, 10, 5, 2, 9, 1, 8]) == 113
+    assert Bowling.score([8, 0, 3, 0, 9, 0, 1, 3, 5, 2, 1, 4, 1, 6, 10, 2, 8, 5, 5, 1]) == 89
+    assert Bowling.score([9, 0, 4, 3, 5, 4, 6, 4, 5, 4, 4, 4, 8, 0, 7, 3, 1, 7, 4, 2]) == 90
+    assert Bowling.score([8, 2, 5, 0, 2, 6, 0, 7, 6, 3, 0, 10, 2, 5, 3, 0, 3, 2, 8, 2, 4]) == 85
+    assert Bowling.score([10, 7, 3, 9, 0, 10, 0, 8, 8, 2, 0, 6, 10, 10, 10, 8, 1]) == 167
+    assert Bowling.score([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 3, 4]) == 24
   end
 end
